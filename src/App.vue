3a83ba5b14a3470e2eb2fa1a7f16cc6cbd73799e
harivenkateshe <harivenkateshe@syncfusion.com>
2021-08-10 18:09:55 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ejs-progressbar id="percentage" :type='type' :showProgressValue='showProgressValue' :value='value' :animation="animation">
+  </ejs-progressbar>
+</template>
+
+<script>
+import { ProgressBarComponent } from "@syncfusion/ej2-vue-progressbar";
+
+export default {
+  name: "App",
+  components: {
+    "ejs-progressbar": ProgressBarComponent,
+  },
+  data() {
+    return {
+      type: 'Circular',
+      showProgressValue: 'true',
+      value: 100,
+      animation: {
+        enable: true,
+        duration: 2000,
+        delay: 0,
+      },
+    };
+  },
+};
+</script>
